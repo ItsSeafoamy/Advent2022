606423f61d7b3941c8bb3762c20a0d8cef2912da
Lax <laxynd@siblaze.com>
2022-12-03 06:14:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advent 2022
+Solutions for Advent of Code 2022, provided by Github's Copilot
+
+I gave myself the challenge this year of only using Copilot to solve all the puzzles, as such, the vast majority of the code was not written by me.  
+An exception to this is the main Advent2022 class, which is used to read the input and then call the appropriate puzzle class.  
+The puzzle inputs, all found in the resources folder, were all manually copied from the AoC website.
+
+## [Advent of Code 2022](https://adventofcode.com/2022)
+
+**Don't look at the code if you want to solve the puzzles yourself!**
---
+++ src/main/java/me/laxynd/advent2022/Advent2022.java
@@ -0,0 +1,18 @@
+package me.laxynd.advent2022;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Advent2022 {
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		int day = 1;
+
+		List<String> input = Files.readAllLines(Paths.get("src", "main", "resources", "day" + day + ".txt"));
+
+		Class.forName("me.laxynd.advent2022.day" + day + ".Main").getMethod("run", List.class).invoke(null, input);
+	}
+}
